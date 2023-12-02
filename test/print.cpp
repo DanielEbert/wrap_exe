b19eaad65538653d06717387c93daa0fad59d554
Daniel Ebert <iuq@web.de>
2023-12-02 02:32:54 +0100

    test print argv

--- test/print.cpp
+++ test/print.cpp
@@ -2,11 +2,17 @@
 #include <chrono>
 #include <thread>
 
-int main()
+int main(int argc, char* argv[])
 {
     while(true)
     {
         std::cout << "ping" << std::endl;
+        for (int i = 0; i < argc; i++)
+        {
+            std::cout << argv[i] << " ";
+        }
+        std::cout << std::endl;
+
         std::this_thread::sleep_for(std::chrono::milliseconds(1000));
     }
 }
